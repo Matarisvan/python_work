d73faa9c8954c7c6915db4c71986c3584fcce266
LuisMa <lupadilla@my365.bellevue.edu>
2022-08-20 02:05:51 -0400

    fyck this

---
+++ andres.py
@@ -0,0 +1,9 @@
+# Luis Padilla
+# 08/17/22
+# CSD205 Module 3 Assignment
+
+print("Welcome!")
+name = input("company name")
+print("Welcome", name.title() + "!")
+cost = (float(input("amount of feet required"))) * .87
+print(name.title(), "$" + str(cost))

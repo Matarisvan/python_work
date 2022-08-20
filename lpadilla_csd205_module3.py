6fa59fc06f8de3b6c44650ad7eb6e87a5e7c8ad4
LuisMa <lupadilla@my365.bellevue.edu>
2022-08-20 17:40:22 -0400

    ready for submission

---
+++ lpadilla_csd205_module3.py
@@ -0,0 +1,10 @@
+# Luis Padilla
+# 08/17/22
+# CSD205 Module 3 Assignment
+
+print("Welcome!")
+name = input("What is your company name? ")
+print("Welcome", name.title() + "!")
+print("The cost of optic fiber cable per foot is $0.87")
+cost = (float(input("How many feet would you like? "))) * .87
+print(name.title() + ",", "Your total cost will be", "$" + str(cost))

ebec9966556aa62dcd1e60ccb5115ac4ee0c8bfd
LuisMa <luismilan21@gmail.com>
2022-09-12 12:58:11 -0400

    ready for submission

--- padilla_csd205_module7.py
+++ padilla_csd205_module7.py
@@ -2,16 +2,19 @@
 # 09/09/22
 # CSD205 Module 7 Assignment
 
-print("Hello! and Welcome to the Investment Calculator!\n\nWe are here to determine how long it takes to double your investment!")
+print("\nHello! and Welcome to the Investment Calculator!\n\nFind out how many years it takes to double your investment at a fixed annual rate!")
 money = float(input("How much money are you investing?\t$"))
-rate = float(input("What is the interest rate?\t\t%")) * .01
+rate = float(input("What is the interest rate?\t\t%"))
+percent = rate * .01
 total = 0
 years = 1
-perYear = money * rate
+perYear = money * percent
+
+print()
 
 while total <= (money * 2):    
     total = total + perYear
-    print(years, "$" + str(total))
+    #print(years, "$" + str(total)) # print this to see each year's total
     years += 1
 print("It took ", years - 1, " years to double your investment at an annually fixed percentage of ", rate,"%")
-print("congratulations!")
+print("Congratulations! Your investment brought in $" + str(perYear), " every year!\n")

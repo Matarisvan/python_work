8961b763d74f01bf846ae9145211a1c7b54de734
LuisMa <luismilan21@gmail.com>
2022-09-17 11:56:15 -0400

    1st rev

---
+++ padilla_csd205_module8.py
@@ -0,0 +1,10 @@
+# Luis Padilla
+# 09/17/22
+# CSD205 Module 8 Assignment
+
+print("\nWelcome to the Miles to Kilometers Converter!\n")
+miles = float(input("How many miles have you driven this past year?"))
+
+
+def drive():
+    kilo = miles * 

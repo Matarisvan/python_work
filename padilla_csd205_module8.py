4a71b549d62e49e4484ace257b7e4bdefa2d6a08
LuisMa <luismilan21@gmail.com>
2022-09-20 11:00:58 -0400

    rev 2

--- padilla_csd205_module8.py
+++ padilla_csd205_module8.py
@@ -2,9 +2,30 @@
 # 09/17/22
 # CSD205 Module 8 Assignment
 
-print("\nWelcome to the Miles to Kilometers Converter!\n")
-miles = float(input("How many miles have you driven this past year?"))
 
+print("\nWelcome to the Miles to Kilometers Converter!\n")
+
+try:
+    miles = float(input("How many miles have you driven this past year? "))
+    kilo = float(miles * 1.609)
+except:
+    print("error")
+print("Every mile that you drive is 1.609 Kilometers!")
 
 def drive():
-    kilo = miles * 
+    miles = float(input("miles"))
+    if miles >=1:
+        print("You have driven", kilo, "kilometers this year!")
+    else:
+        print("That is an impossible amount. Please Try again!\t")
+        drive()
+drive()
+
+
+
+'''
+if miles >= 1:
+    pass
+else:
+    drive()
+'''

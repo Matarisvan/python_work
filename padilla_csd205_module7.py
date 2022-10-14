984916e046569385a15e09799037fa9058205ee0
LuisMa <lupadilla@my365.bellevue.edu>
2022-10-14 12:54:15 -0400

    mod7 fixed

--- padilla_csd205_module7.py
+++ padilla_csd205_module7.py
@@ -6,15 +6,16 @@ print("\nHello! and Welcome to the Investment Calculator!\n\nFind out how many y
 money = float(input("How much money are you investing?\t$"))
 rate = float(input("What is the interest rate?\t\t%"))
 percent = rate * .01
-total = 0
-years = 1
+total = money
+years = 0
 perYear = money * percent
 
 print()
 
 while total <= (money * 2):    
-    total = total + perYear
+    total += perYear
+    perYear = total * percent
     #print(years, "$" + str(total)) # print this to see each year's total
     years += 1
-print("It took ", years - 1, " years to double your investment at an annually fixed percentage of ", rate,"%")
+print("It took ", years, " years to double your investment at an annually fixed percentage of ", rate,"%\nYou now have $", total, "!")
 print("Congratulations! Your investment brought in $" + str(perYear), " every year!\n")
